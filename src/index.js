d92ce6425ef7afd984e83e6410e8910d46333fb4
Davi Cheli Miquelim <davi.miquelim@bolttech.io>
2023-01-10 20:20:27 -0300

    chore: change client uri to server productiom

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://odyssey-lift-off-client-production-8b4a.up.railway.app/", // change to YOUR own production server
+  uri: "https://odyssey-lift-off-server-production-572d.up.railway.app/", // change to YOUR own production server
   cache: new InMemoryCache(),
   name: "web",
   version: "1.0",
